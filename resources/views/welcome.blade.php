148c980f2f62b44812edb401b418844378543266
Ryan Hayden <ryanhayden@Ryans-MacBook-Air.local>
2022-12-26 15:57:36 -0600

    header in progress

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,110 +10,38 @@
 <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,400&family=Open+Sans:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
          @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body >
-        <main class="container">
-            <header>
-            <nav>
+    <body class="home">
+        <header>
+            <section class="top" style="background: linear-gradient(rgba(0,0,0, .5), rgba(0,0,0, .5)), url('/homephoto.jpg');">
+            <nav class="main-navigation">
+                <a href="#" class="logo">Bible Baptist Church</a> 
                 <ul>
-                    <li><strong>Bible Baptist Church</strong></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Ministries</a></li>
+                    <li><a href="#">Sermons</a></li>
                 </ul>
-                <ul>
-                    <li><a href="#">Watch Live</a></li>
-                    <li><a href="#">Statement of Faith</a></li>
-                    <li><a href="#" role="button">Give Online</a></li>
-                </ul>
+                <button class="hamburger">Menu</button>
             </nav>
-            <section class="hero">
-                <hgroup>
-                <h1>A Home For Your Family</h1>
-                <h2>We are a community of believers in Mattoon, IL who meet together to <mark>worship</mark> God, to <mark>study</mark> God's word, to <mark>serve</mark> each other and to <mark>make disciples</mark> for Jesus Christ.</h2>
-                </hgroup>
-            </section>
-            </header>
-            <main>
-            <section class="distinctives">
-            <h2>Our distinctives</h2>
-            <div class="grid">
-            <article>
-            <header>
-            <h3>Expository Preaching</h3>
-            </header>
-            <p>We center our services around the teaching and preaching of the Bible.  We believe the Bible itself has power and that when it is clearly explained, God works through it to build up His people.</p>
-            </article>
-            <article>
-            <header>
-            <h3>Fellowship</h3>
-            </header>
-            <p>We take fellowship seriously.  We meet together for fellowship as a church at least once a month and have various groups for fellowship during each week.  Almost every service, people stay long after to fellowship with each other.</p>
-            </article>
-            <article>
-            <header>
-            <h3>Family Focused</h3>
-            </header>
-            Bible Baptist is a church full of young families and we try to provide resources for families to raise their kids for God.  These include our youth group, family sunday school classes, awanas and Trail Life clubs.
-            </article>
+            <h1>A church home for you and your family.</h1>
+        </section>
+        
+        <section class="actions">
+            <div>
+                <a href="#">Learn More</a>
             </div>
-            
-            </section>
-            <section class="ministries">
-            <h2>Our Ministries</h2>
-            <div class="grid">
-            <article>
-            <header>
-            <h3>Sunday School</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Seed Sowing</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Music</h3>
-            </header>
-            </article>
+            <div>
+                <a href="#">Watch Live</a>
             </div>
-            <div class="grid">
-            <article>
-            <header>
-            <h3>Youth Group</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Trail Life</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Awanas</h3>
-            </header>
-            </article>
+            <div>
+                <a href="#">Give Online</a>
             </div>
-            </section>
-            <section class="leadership">
-            <h2>Our Leadership</h2>
-            <div class="grid">
-            <article>
-            <header>
-            <h3>Ryan Hayden</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Adam McCaslin</h3>
-            </header>
-            </article>
-            <article>
-            <header>
-            <h3>Ken Hedrick</h3>
-            </header>
-            </article>
-            </div>
-            
-            </section>
-            </main>
+        </section>
+        </header>
+        <main>
+
         </main>
+        <footer>
+
+        </footer>
     </body>
 </html>
