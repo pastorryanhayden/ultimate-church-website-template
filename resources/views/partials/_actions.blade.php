c2578a8019aa30e9692b5f8b7f008f83dba109fd
Ryan Hayden <pastorryanhayden@gmail.com>
2022-12-28 15:24:00 -0600

    removed dead links from menu

--- resources/views/partials/_actions.blade.php
+++ resources/views/partials/_actions.blade.php
@@ -11,4 +11,3 @@
     <p>For Members</p>
         <a href="https://tithe.ly/give?c=247580" target="blank">Give Online</a>
     </div>
-</section>
--- resources/views/partials/_menu.blade.php
+++ resources/views/partials/_menu.blade.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href="/about">About Us</a></li>
-    <li><a href="#">Ministries</a></li>
-    <li><a href="#">Sermons</a></li>
+    {{-- <li><a href="#">Ministries</a></li>
+    <li><a href="#">Sermons</a></li> --}}
 </ul>
