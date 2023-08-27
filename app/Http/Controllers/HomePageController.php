d7fd052523e53f0c554b3577b754507c9a7936d5
Ryan Hayden <pastorryanhayden@gmail.com>
2023-08-27 06:13:58 -0500

    changed homepage to show events for today too

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -12,9 +12,11 @@ class HomePageController extends Controller
     public function show()
     {
         $today = now();
+        // Get yesterday's date
+        $yesterday = $today->subDays(1);
         $ministries = Ministry::where('homepage', true)->limit(4)->get();
         // Get the next upcoming event that is on the homepage
-        $event = Event::where('start_date', '>=', $today)->where('on_homepage', true)->orderBy('start_date', 'asc')->first();
+        $event = Event::where('start_date', '>=', $yesterday)->where('on_homepage', true)->orderBy('start_date', 'asc')->first();
         // Get all the devotions published in the last 30 days
         $recent_devotions = Devotion::where('published', true)->where('published_at', '>=', $today->subDays(30))->orderBy('created_at', 'desc')->limit(6)->get();   
         return view('welcome', compact('ministries', 'event', 'recent_devotions'));
