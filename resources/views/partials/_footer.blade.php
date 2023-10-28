4a339617f0dfac59ee3513c5dc40535eff46416b
Ryan Hayden <ryanhayden@Ryans-MacBook-Air.local>
2023-10-28 07:49:53 -0500

    couple fixes

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
  <footer>
  @if($show_map)
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3079.6823770347582!2d-88.40071454861499!3d39.47650367938475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x887311fe472b780f%3A0x380b6224f17e358f!2sBible%20Baptist%20Church!5e0!3m2!1sen!2sus!4v1672189972888!5m2!1sen!2sus" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="{{$site_global->map_url}}" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 @endif 
         <hgroup>
         <h2>Experience Bible Baptist</h2>
