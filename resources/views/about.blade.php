b10d9f7c14d39876410fb29e9996dfbe38617471
Ryan Hayden <pastorryanhayden@gmail.com>
2024-04-24 08:18:08 -0500

    removed more hard coding

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -31,14 +31,10 @@
 @endforeach
 </section>
 @endif 
+@if($about->what_church_like)
 <section>
-<h2>What is your church like?</h2>
-<h3>Friendly</h3>
-<p>Bible Baptist church is a friendly church made up of a bunch of imperfect people who really love each other and try to serve each other.  You'll often find our people fellowshipping together at a local restaurant. It's not uncommon to find them still fellowshipping together an hour after a service has ended. Despite their closeness, they will welcome you in and make you a part of the family.</p>
-<h3>Multi-generational</h3>
-<p>BBC is a church full of young people and young families. Our nurseries are full and it's very common to hear babies making sounds during a service.  On any given sunday, you'll worship with children as young as 2 and people as old as 92. </p>
-<h3>Diverse</h3>
-<p>We believe that the church is best when it reflects the diversity of God's people.  In our church you'll find rich and poor, educated and drop outs, people of all ages and from many different walks of life worshipping Christ together.</p>
+<h2>{{ $about->what_church_like_title }}</h2>
+{!! $about->what_church_like !!}
 </section>
 <section>
 <h2>Other Information</h2>
@@ -49,5 +45,5 @@
 <li><a href="/documents/constitution.pdf" target="blank">Church Constituation</a></li>
 </ul>
 </section>
-
+@endif 
 @endsection
