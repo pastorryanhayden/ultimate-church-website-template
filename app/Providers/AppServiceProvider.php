d78597c56befa707fd24362af39479d586b2f0ba
Shift <shift@laravelshift.com>
2024-07-12 17:07:04 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,11 +5,23 @@ namespace App\Providers;
 use App\Models\ChapterSermon;
 use App\Observers\ChapterSermonObserver;
 use App\Services\MarkdownService;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to your application's "home" route.
+     *
+     * Typically, users are redirected here after authentication.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
     /**
      * Register any application services.
      *
@@ -33,5 +45,14 @@ class AppServiceProvider extends ServiceProvider
             return "<?php echo app(App\Services\MarkdownService::class)->toHtml($expression); ?>";
         });
         ChapterSermon::observe(ChapterSermonObserver::class);
+
+        $this->bootRoute();
+    }
+
+    public function bootRoute(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
