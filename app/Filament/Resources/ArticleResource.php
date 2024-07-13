d7cf42ab6fd83b2adf006cd8aa8139d65293c5aa
Ryan Hayden <pastorryanhayden@gmail.com>
2024-07-12 21:17:41 -0500

    wip

--- app/Filament/Resources/ArticleResource.php
+++ app/Filament/Resources/ArticleResource.php
@@ -4,10 +4,18 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ArticleResource\Pages;
 use App\Models\Article;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Support\Str;
 
 class ArticleResource extends Resource
 {
@@ -21,7 +29,37 @@ class ArticleResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Section::make()
+                    ->columns(2)
+                    ->schema([
+                        TextInput::make('title')
+                            ->live(onBlur: true)
+                            ->required()
+                            ->afterStateUpdated(function (Get $get, Set $set, ?string $old, ?string $state) {
+                                if (($get('slug') ?? '') !== Str::slug($old)) {
+                                    return;
+                                }
+
+                                $set('slug', Str::slug($state));
+                            }),
+
+                        TextInput::make('slug')
+                            ->required(),
+                        Textarea::make('description')
+                            ->columnSpanFull()
+                            ->required(),
+                        FileUpload::make('featured_image')
+                            ->disk('vultr')
+                            ->directory('images')
+                            ->visibility('public')
+                            ->image()
+                            ->columnSpanFull(),
+
+                    ]),
+                MarkdownEditor::make('content')
+                    ->columnSpanFull()
+                    ->required(),
+
             ]);
     }
 
